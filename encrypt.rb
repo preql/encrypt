ee9057e536da98b55f2a52f3afaf0353af48cacd
Eugeny Sobolenko <stereotype1@mail.ru>
2019-07-09 21:25:59 +0400

    initialize

---
+++ encrypt.rb
@@ -0,0 +1,23 @@
+require "digest"
+
+puts "Введите слово или фразу для шифрования:"
+
+user_input = STDIN.gets.chomp
+
+puts "Каким способом зашифровать:"
+puts "1. MD5"
+puts "2. SHA1"
+user_choice_method = STDIN.gets.to_i
+
+until user_choice_method.between?(1, 2)
+  puts "Выберите 1 или 2"
+  user_choice_method = STDIN.gets.to_i
+end
+
+puts "Вот что получилось:"
+case user_choice_method
+when 1 then
+  puts Digest::MD5.hexdigest(user_input)
+when 2 then
+  puts Digest::SHA1.hexdigest(user_input)
+end
